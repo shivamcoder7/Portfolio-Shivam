251ffd84299d2cc9efaeca4bd13a75a34b30d4e0
Shivam <cpcoder7777@gmail.com>
2024-02-05 13:01:51 +0530

    Image get circled, added intro

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
+import Header from "./Components/Header";
 
 function App() {
   return (
     <div className="App">
       <h1>Hare Krishna</h1>
-      <img src="/images/Manali_Pic.jpeg" alt="Shivam_Image"/>
+      <Header/>
     </div>
   );
 }
